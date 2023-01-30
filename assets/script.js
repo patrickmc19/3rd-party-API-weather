9d3ea7081422cfa95a7b0a9a213e3d4adb629b4e
Patrick McKnight <patrickgmcknight@gmail.com>
2023-01-29 21:27:24 -0800

    added jQuery, dayjs, researched data returned from weather api call

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,28 @@
 var weatherAPI = '127337ecf2639c829a50c27f0e45f8c0';
 var weatherURL = 'http://api.openweathermap.org/data/2.5/weather';
+var city = document.getElementById("#cityinput");
+var search = $("#search");
+var ulCity = $("#ul");
+
+function getAPI() {
+    // "coord.lon" - geocodes city longitude
+    // "coord.lat" - geocodes city latitude
+    // "main.temp" - gathers city temp
+    // "wind.speed" - gathers city wind speed
+    // "main.humidity" - gathers city humidity
+    // "weather.main" - use to create if statement for visual weather icon
+}
+
+function getWeather() {
+    fetch(weatherURL)
+    .then(function (response){
+        return response.json();
+    })
+    .then(function (data){
+        for (let i = 0; i < array.length; i++) {
+            
+        }
+    })
+}
+
+search.addEventListener("click", getWeather)
