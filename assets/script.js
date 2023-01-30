5e43f31494ecdd137e6949ee223dd6100ff8da9c
Patrick McKnight <patrickgmcknight@gmail.com>
2023-01-30 15:34:38 -0800

    added geocode api to pull lat/lom

--- assets/script.js
+++ assets/script.js
@@ -1,28 +1,25 @@
 var weatherAPI = '127337ecf2639c829a50c27f0e45f8c0';
 var weatherURL = 'http://api.openweathermap.org/data/2.5/weather';
+var geocodeAPI = 'http://api.openweathermap.org/geo/1.0/direct?q=';
 var city = document.getElementById("#cityinput");
 var search = $("#search");
 var ulCity = $("#ul");
-
-function getAPI() {
-    // "coord.lon" - geocodes city longitude
-    // "coord.lat" - geocodes city latitude
-    // "main.temp" - gathers city temp
-    // "wind.speed" - gathers city wind speed
-    // "main.humidity" - gathers city humidity
-    // "weather.main" - use to create if statement for visual weather icon
-}
+var today = dayjs().format();
 
 function getWeather() {
-    fetch(weatherURL)
-    .then(function (response){
-        return response.json();
-    })
-    .then(function (data){
-        for (let i = 0; i < array.length; i++) {
-            
-        }
-    })
+    var city = $("#aside").children("input").val();
+    fetch(geocodeAPI + city + "&limit=1&appid=" + weatherAPI)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            var lon = data.lon;
+            var lat = data.lat;
+            fetch(weatherURL + "?lat=" + lat + "&lon" + lon + "&appid=" + weatherAPI);
+            // var currentTemp = data.main.temp;
+            // var currentWind = data.wind.speed;
+            // var currentHumidity = data.main.humidity;
+        })
 }
 
-search.addEventListener("click", getWeather)
+$("#city-input").on("click", getWeather)
