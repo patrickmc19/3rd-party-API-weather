affaa84974359b1c76ad42bf656df5fbd01eaa43
Patrick McKnight <patrickgmcknight@gmail.com>
2023-02-01 13:30:22 -0800

    updated API calls, added loop for forecast, created cards to display forecast, added icon for weather

--- assets/script.js
+++ assets/script.js
@@ -1,43 +1,43 @@
 var weatherAPI = '127337ecf2639c829a50c27f0e45f8c0';
-var weatherURL = 'http://api.openweathermap.org/data/2.5/weather';
-var geocodeAPI = 'http://api.openweathermap.org/geo/1.0/direct?q=';
-var city = document.getElementById("#cityinput");
+var weatherURL = 'https://api.openweathermap.org/data/2.5/weather?q=';
+var fiveDay = 'https://api.openweathermap.org/data/2.5/forecast?q='
 var search = $("#search");
-var ulCity = $("#ul");
-var today = dayjs().format();
 
-function getCoords() {
-    navigator.geolocation.getCurrentPosition(success);
+// on load set item and return previous searches as buttons.
+
+function getWeather() {
     var city = $("#aside").children("input").val();
-    fetch(geocodeAPI + city + "&limit=1&appid=" + weatherAPI)
+    // set item for city searched
+    fetch(fiveDay + city + "&units=imperial&limit=1&appid=" + weatherAPI)
         .then(function (response) {
-            console.log(response)
             return response.json();
         })
         .then(function (data) {
-            console.log(data)
-            var lon = data.lon;
-            console.log(lon);
-            var lat = data.lat;
-            console.log(lat);
-            fetch(weatherURL + "?lat=" + lat + "&lon" + lon + "&appid=" + weatherAPI)
+            for (let i = 2; i < data.list.length; i = i + 8) {
+                $("#five-day-card").append(`<div class="col-sm-2">
+                <div class="card">
+                  <div class="card-body">
+                    <h5 class="card-title">Special title treatment</h5>
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                  </div>
+                </div>
+              </div>`)
+            }
+            fetch(weatherURL + city + "&units=imperial&appid=" + weatherAPI)
                 .then(function (response2) {
                     return response2.json();
                 })
-            })
-                .then (function (data2) {
-                    console.log(data2)
-                var currentTemp = data2.main.temp;
-                console.log(currentTemp)
-                var currentWind = data2.wind.speed;
-                console.log(currentWind)
-                var currentHumidity = data2.main.humidity;
-                console.log(currentHumidity)
+                .then(function (data2) {
+                    $("#city-name").append(`${data2.name} ${moment(data2.dt, "X").format('MM/DD/YYYY')} <img id="weather-icon" src= "http://openweathermap.org/img/wn/${data2.weather[0].icon}@2x.png">`)
+                    var currentTemp = data2.main.temp;
+                    $("#temp").text("Temp: " + currentTemp);
+                    var currentWind = data2.wind.speed;
+                    $("#wind").text("Wind: " + currentWind);
+                    var currentHumidity = data2.main.humidity;
+                    $("#humidity").text("Humidity: " + currentHumidity)
                 })
+        })
 }
 
-function getWeatherData() {
-
-}
-
-$("#search").on("click", getCoords)
+$("#search").on("click", getWeather)
