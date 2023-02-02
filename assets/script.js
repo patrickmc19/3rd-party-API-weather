46bf8cde41e16c8223f531bd745e304e4403a59c
Patrick McKnight <patrickgmcknight@gmail.com>
2023-02-01 19:16:51 -0800

    corrected error where results continue to append.

--- assets/script.js
+++ assets/script.js
@@ -1,41 +1,40 @@
-var weatherAPI = '127337ecf2639c829a50c27f0e45f8c0';
-var weatherURL = 'https://api.openweathermap.org/data/2.5/weather?q=';
-var fiveDay = 'https://api.openweathermap.org/data/2.5/forecast?q='
+var API = '127337ecf2639c829a50c27f0e45f8c0';
+var currentWeather = 'https://api.openweathermap.org/data/2.5/weather?q=';
+var fiveDayForecast = 'https://api.openweathermap.org/data/2.5/forecast?q='
 var search = $("#search");
-
-// on load set item and return previous searches as buttons.
 
 function getWeather() {
     var city = $("#aside").children("input").val();
-    // set item for city searched
-    fetch(fiveDay + city + "&units=imperial&limit=1&appid=" + weatherAPI)
+    var id = "prev-cities";
+    localStorage.setItem(id, prevCity);
+    fetch(fiveDayForecast + city + "&units=imperial&limit=1&appid=" + API)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
-            for (let i = 2; i < data.list.length; i = i + 8) {
-                $("#five-day-card").append(`<div class="col-sm-2">
-                <div class="card">
-                  <div class="card-body">
-                    <h5 class="card-title">Special title treatment</h5>
-                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                  </div>
-                </div>
-              </div>`)
+            for (let i = 0; i < data.list.length; i = i + 8) {
+                $("#day-" + i).empty();
+                $("#temp-" + i).empty();
+                $("#wind-" + i).empty();
+                $("#humidity-" + i).empty();
+                $("#day-" + i).append(`${moment(data.list[i].dt, "X").format('MM/DD/YYYY')} <img id="weather-icon1" src= "http://openweathermap.org/img/wn/${data.list[i].weather[0].icon}@2x.png">`);
+                $("#temp-" + i).append(" " + `${data.list[i].main.temp}` + "°F");
+                $("#wind-" + i).append(" " + `${data.list[i].wind.gust}` + " MPH");
+                $("#humidity-" + i).append(" " + `${data.list[i].main.humidity}` + " %");
             }
-            fetch(weatherURL + city + "&units=imperial&appid=" + weatherAPI)
+            fetch(currentWeather + city + "&units=imperial&appid=" + API)
                 .then(function (response2) {
                     return response2.json();
                 })
                 .then(function (data2) {
+                    $("#city-name").empty();
                     $("#city-name").append(`${data2.name} ${moment(data2.dt, "X").format('MM/DD/YYYY')} <img id="weather-icon" src= "http://openweathermap.org/img/wn/${data2.weather[0].icon}@2x.png">`)
                     var currentTemp = data2.main.temp;
-                    $("#temp").text("Temp: " + currentTemp);
+                    $("#temp").text("Temp: " + currentTemp + "°F");
                     var currentWind = data2.wind.speed;
-                    $("#wind").text("Wind: " + currentWind);
+                    $("#wind").text("Wind: " + currentWind + " MPH");
                     var currentHumidity = data2.main.humidity;
-                    $("#humidity").text("Humidity: " + currentHumidity)
+                    $("#humidity").text("Humidity: " + currentHumidity + " %")
                 })
         })
 }
